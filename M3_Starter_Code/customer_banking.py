9a1d35aa5b0094a131b6979e4c937ca58e19de52
Chris Tan <152225639+tanchris232@users.noreply.github.com>
2023-12-18 18:02:20 -0500

    Create the SAF complete.

--- M3_Starter_Code/customer_banking.py
+++ M3_Starter_Code/customer_banking.py
@@ -1,6 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
-
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 # Define the main function
 def main():
     """This function prompts the user to enter the savings and cd account balance, interest rate,
